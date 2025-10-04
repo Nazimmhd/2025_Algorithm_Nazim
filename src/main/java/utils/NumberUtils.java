8ecea90ccdb4f59fc92b175baccea2ad399ae3c5
Nazim <Nazimmhd786@gmail.com>
2025-10-04 01:30:39 +0100

    get Pos

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -1,4 +1,16 @@
 package utils;
 
 public class NumberUtils {
+
+    /** find the first pos of a value in an array
+     *
+      */
+
+    public static int getPos(int[] nums,int value){
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]==value){
+                return i;
+            }
+        }return -1;
+    }
 }
