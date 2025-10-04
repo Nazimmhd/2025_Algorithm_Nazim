ffc99338ce1b03d3a01f58bfddd730544ff5ae2a
Nazim <Nazimmhd786@gmail.com>
2025-10-04 12:35:19 +0100

    get Pos

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -3,7 +3,9 @@ package utils;
 public class NumberUtils {
 
     /** find the first pos of a value in an array
-     *
+     * @param nums the array of integers
+     * @param value the value to find
+     * @return  The index of the first occurence, or -1 if not found
       */
 
     public static int getPos(int[] nums,int value){
