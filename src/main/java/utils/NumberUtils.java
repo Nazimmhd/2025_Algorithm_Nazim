5434cfaccb0b0ac987247bc901fd67e094e6df24
Nazim <Nazimmhd786@gmail.com>
2025-10-04 16:54:53 +0100

    get Pos  get lastPos  is identical

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -35,5 +35,27 @@ public class NumberUtils {
 
     }
 
+    /**
+     * checking if the arrays are identical
+     * @param a1 first array
+     * @param a2 second array
+     *@return true if the arrays are identical
+     */
+
+    public static boolean isIdentical(int[]a1,int[]a2){
+        if(a1.length!=a2.length)
+            return false;
+        for(int i=0;i<a1.length;i++){
+            if(a1[i]!=a2[i])
+               return false;
+        }
+        return true;
+    }
+
+
+
+
+
+
 
 }
